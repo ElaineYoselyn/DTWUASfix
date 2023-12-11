dd392570062d58822fe724c51d8d04d19a3dbe8d
Kaishar727 <williambunaidy3@gmail.com>
2023-12-11 17:00:34 +0700

    test

--- assets/js/review-section.js
+++ assets/js/review-section.js
@@ -39,5 +39,29 @@ $(".custom-carousel").owlCarousel({
     $(".nav").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
     $(".productnavbar").toggleClass("mobile-nav");
-    $("#audiomenu").slideToggle(); // Add this line
+    $("#audiomenu").slideToggle();
   });
+  
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.nav-item');
+
+function handleIndicator(el) {
+  items.forEach(item => {
+    item.classList.remove('is-active');
+    item.removeAttribute('style');
+  });
+  
+  indicator.style.width = `${el.offsetWidth}px`;
+  indicator.style.left = `${el.offsetLeft}px`;
+  indicator.style.backgroundColor = el.getAttribute('active-color');
+
+  el.classList.add('is-active');
+  el.style.color = el.getAttribute('active-color');
+}
+
+
+items.forEach((item, index) => {
+  item.addEventListener('click', (e) => { handleIndicator(e.target)});
+  item.classList.contains('is-active') && handleIndicator(item);
+});
+  // Add an event listener to each menu item in the mobile navbar
